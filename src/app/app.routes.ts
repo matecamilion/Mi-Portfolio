4cca5edb5393b039f43f03ace3b2812ee8f295b2
matecamilion <mateocamilion@gmail.com>
2026-04-19 23:12:24 -0300

    Primer Commit - Generando Mi Portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { SobreMi } from './pages/sobre-mi/sobre-mi';
+import { Educacion } from './pages/educacion/educacion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home},
+    {path: 'sobre-mi', component: SobreMi},
+    {path: 'educacion', component: Educacion}
+];
