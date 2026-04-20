2046fed9555747ca1a0893f82e2b343cc52a4e06
matecamilion <mateocamilion@gmail.com>
2026-04-20 15:59:01 -0300

    formularo de contacto funcional con validaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { SobreMi } from './pages/sobre-mi/sobre-mi';
 import { Educacion } from './pages/educacion/educacion';
+import { Habilidades } from './pages/habilidades/habilidades';
+import { Proyectos } from './pages/proyectos/proyectos';
+import { Experiencia } from './pages/experiencia/experiencia';
+import { Contacto } from './pages/contacto/contacto';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: Home},
     {path: 'sobre-mi', component: SobreMi},
-    {path: 'educacion', component: Educacion}
+    {path: 'educacion', component: Educacion},
+    {path: 'habilidades', component: Habilidades},
+    {path: 'proyectos', component: Proyectos},
+    {path: 'experiencia', component: Experiencia},
+    {path: 'contacto', component: Contacto}
 ];
--- src/app/pages/contacto/contacto.ts
+++ src/app/pages/contacto/contacto.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [ReactiveFormsModule, ],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
 export class Contacto {
+  private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
+
+  contactForm: FormGroup = this.fb.group({
+    nombre:  ['', Validators.required],
+    email:   ['', [Validators.required, Validators.email]],
+    mensaje: ['', Validators.required]
+  });
+
+  enviado = false;
+
+  onSubmit() {
+    if (this.contactForm.invalid) return;
+
+    this.http.post(
+      'https://formspree.io/f/maqadyvd',
+      this.contactForm.value,
+      { headers: { 'Accept': 'application/json' } }
+    ).subscribe({
+      next: () => {
+        this.enviado = true;
+        this.contactForm.reset();
+      },
+      error: (err: any) => console.error(err)
+    });
+  }
 
 }
