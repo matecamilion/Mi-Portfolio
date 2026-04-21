aa41c9d52bcdadace5c02231c2056644a85d07cc
matecamilion <mateocamilion@gmail.com>
2026-04-21 10:10:55 -0300

    Agregamos footer, y detalles tecnicos de scroll para el header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Footer } from "./shared/footer/footer";
+import { SobreMi } from './pages/sobre-mi/sobre-mi';
+import { Educacion } from './pages/educacion/educacion';
+import { Experiencia } from './pages/experiencia/experiencia';
+import { Habilidades } from './pages/habilidades/habilidades';
+import { Home } from './pages/home/home';
+import { Proyectos } from './pages/proyectos/proyectos';
+import { Contacto } from './pages/contacto/contacto';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, SobreMi, Educacion, Experiencia, Habilidades, Home, Proyectos, Contacto, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -3,10 +3,28 @@ import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
+  menuOpen = false;
+
+  scroll(id: string) {
+  const element = document.getElementById(id);
+
+  if (element) {
+    const headerOffset = 20; // altura aprox de tu header
+    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  }
+
+  this.menuOpen = false;
+}
 }
